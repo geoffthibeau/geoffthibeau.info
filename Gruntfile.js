5c066f87f2417c75356c9792cbaa93ec54019af8
Geoff Thibeau <geoff@deepfriedads.com>
2018-04-26 21:37:16 -0500

    Removes older Internet Explorer conditionals. Updates html5shiv. Removes Knowledge section. Adds Director of Interactive position. Removes employment summaries. Updates copyright years. Redirects portfolio.html to homepage. Removes portfolio images. Updates package.json and Gruntfile. Removes dependency on compass.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,65 +1,86 @@
 module.exports = function( grunt )
 {
-  // Project configuration.
-  grunt.initConfig(
-  {
-    pkg : grunt.file.readJSON( 'package.json' )
-    , compass :
-    {
-      dist :
-      {
-          options :
-          {
-            config : 'config/config.rb'
-          }
-      }
-    }
-    , postcss :
-    {
-      options :
-      {
-        map :
-        {
-          inline : false
-          , prev : 'css/'
-          , annotation : 'css/'
-        }
-        , processors :
-        [
-          require( 'autoprefixer-core' )( { browsers : 'last 2 version, IE > 7' } )
-        ]
-      }
-      , dist :
-      {
-        src : [ 'style.css', 'css/*.css' ]
-      }
-    }
-    , watch :
-    {
-      sass :
-      {
-        files : [ 'scss/*.scss' ]
-        , tasks : [ 'compass', 'postcss' ]
-      }
-      , livereload :
-      {
-        options :
-        {
-          livereload : true
-        }
-        , files :
-          [ 'style.css', 'css/*' ]
-      }
-    }
-  });
+	// grunt.loadNpmTasks( all the grunt things )
+	// https://github.com/sindresorhus/load-grunt-tasks
+	require( 'load-grunt-tasks' )( grunt );
 
-  grunt.loadNpmTasks( 'grunt-contrib-compass' ); // https://github.com/gruntjs/grunt-contrib-compass
-  grunt.loadNpmTasks( 'grunt-contrib-watch' ); // https://github.com/gruntjs/grunt-contrib-watch
-  grunt.loadNpmTasks( 'grunt-postcss' ); // https://github.com/nDmitry/grunt-postcss
-  grunt.loadNpmTasks( 'grunt-notify' ); // https://github.com/dylang/grunt-notify
+	// Project configuration.
+	grunt.initConfig(
+	{
+		pkg : grunt.file.readJSON( 'package.json' ),
+		sass :
+		{
+			options :
+			{
+				sourceMap : true,
+				outputStyle : 'expanded',
+				precision : 5,
+			},
+			dist :
+			{
+				files :
+				{
+					'css/style.css' : 'scss/style.scss',
+				},
+			},
+		},
+		postcss :
+		{
+			options :
+			{
+				map :
+				{
+					inline : false,
+					prev : 'css/',
+					annotation : 'css/',
+				},
+				processors :
+				[
+					require( 'autoprefixer' )(
+					{
+						browsers : 'last 2 version, IE > 7'
+					} ),
+					require( 'cssnano' )(),
+				],
+			},
+			dist :
+			{
+				src : [
+					'style.css',
+					'css/*.css',
+				],
+			},
+		},
+		watch :
+		{
+			css :
+			{
+				files :
+				[
+					'scss/*.scss',
+				],
+				tasks :
+				[
+					'sass',
+					'postcss',
+				],
+			},
+			livereload :
+			{
+				options :
+				{
+					livereload : true,
+				},
+				files :
+				[
+					'css/*.css',
+				],
+			},
+		},
+	});
 
-  // Default task(s).
-  grunt.registerTask( 'default', [ 'watch' ] );
-  grunt.registerTask( 'css', [ 'compass', 'postcss' ] );
+	// Default task(s).
+	grunt.registerTask( 'default', [ 'watch' ] );
+	grunt.registerTask( 'css', [ 'sass', 'postcss' ] );
 
 };
--- README.md
+++ README.md
@@ -3,4 +3,4 @@ geoffthibeau.info
 
 My interactive resume.
 
-Copyright 2014&ndash;2016 Geoff Thibeau unless otherwise noted. All rights reserved.
+Copyright 2014&ndash;2018 Geoff Thibeau unless otherwise noted. All rights reserved.
